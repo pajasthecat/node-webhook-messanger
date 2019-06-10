1c2597c4404fbc0045ebfa5de75949032b0e5a31
pajasthecat <henrik.forsberg1990@gmail.com>
2019-06-10 20:37:49 +0200

    Added callback

--- src/controllers/webhook.js
+++ src/controllers/webhook.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const request = require('request');
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const router = express.Router();
 
@@ -27,6 +28,11 @@ router.post("/", (req, res) => {
       console.log(webhook_event);
       var sender_psid = webhook_event.sender.id;
       console.log(sender_psid);
+      if(webhook_event.message){
+        handleMessage(sender_psid, webhook_event.message)
+      } else if(webhook_event.postback){
+        handlePostBack(sender_psid, webhook_event.postback)
+      }
     });
 
     res.status(200).send("Hello world");
@@ -35,9 +41,37 @@ router.post("/", (req, res) => {
   }
 });
 
-function handleMessage(sender_psid, sender_message) {}
+function handleMessage(sender_psid, received_message) {
+  var response;
+  if(received_message.text){
+    response = {
+      "text": `You said "${received_message.text}"`
+    }
+    callSendAPI(sender_psid, response)
+  }
+}
 
 function handlePostBack(sender_psid, received_postback) {}
 
-function callSendAPI(sender_psid, response) {}
+function callSendAPI(sender_psid, response) {
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
+}
 module.exports = router;
