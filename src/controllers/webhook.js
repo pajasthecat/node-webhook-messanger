4b1070f4dceb171f9d65d6c3fb85c214361cbe05
pajasthecat <henrik.forsberg1990@gmail.com>
2019-06-10 20:17:45 +0200

    Added logging

--- src/controllers/webhook.js
+++ src/controllers/webhook.js
@@ -1,17 +1,6 @@
 const express = require("express");
-
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const router = express.Router();
-
-router.post("/", (req, res) => {
-  var body = req.body;
-  if (body.object == "page") {
-    body.entry.forEach(function(entry) {
-      var webhook_event = entry.messaging[0].message;
-      console.log(webhook_event);
-    });
-  }
-  res.status(200).send("Hello world");
-});
 
 router.get("/", (req, res) => {
   var verifyToken = process.env.SECRET_KEY;
@@ -25,8 +14,30 @@ router.get("/", (req, res) => {
       console.log("Verified");
       res.status(200).send(challenge);
     }
+  } else {
+    res.sendStatus(403);
   }
-  res.sendStatus(403);
 });
 
+router.post("/", (req, res) => {
+  var body = req.body;
+  if (body.object == "page") {
+    body.entry.forEach(function(entry) {
+      var webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+      var sender_psid = webhook_event.sender.id;
+      console.log(sender_psid);
+    });
+
+    res.status(200).send("Hello world");
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+function handleMessage(sender_psid, sender_message) {}
+
+function handlePostBack(sender_psid, received_postback) {}
+
+function callSendAPI(sender_psid, response) {}
 module.exports = router;
